80fe009dba038997cf65417d0db270f1dc97625a
Kevin Yan <kevinyan@169-231-23-231.wireless.ucsb.edu>
2024-10-08 18:11:41 -0700

    test

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -27,7 +27,7 @@ public class DeveloperTest {
 
     @Test
     public void getGithubId_returns_correct_name() {
-        assertEquals("kev-ya", Developer.getName());
+        assertEquals("kev-yan", Developer.getName());
     }
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
